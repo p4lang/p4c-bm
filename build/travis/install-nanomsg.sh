316430db00cd3817ff0df8a2377d08cefcf3dd57
Antonin Bas <antonin@barefootnetworks.com>
2015-08-10 12:09:50 -0700

    do not install thrift and nanomsg locally in travis build (for now)

--- build/travis/install-nanomsg.sh
+++ build/travis/install-nanomsg.sh
@@ -3,6 +3,7 @@ set -e
 wget http://download.nanomsg.org/nanomsg-0.5-beta.tar.gz
 tar -xzvf nanomsg-0.5-beta.tar.gz
 cd nanomsg-0.5-beta
-./configure --prefix=$HOME/nanomsg
+./configure
+# ./configure --prefix=$HOME/nanomsg
 make && sudo make install
 cd ..
