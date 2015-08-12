816de96b4ec55ec91d4ebfb73ac370435348b0fb
Antonin Bas <antonin@barefootnetworks.com>
2015-08-12 07:49:16 -0700

    removed need for sudo in travis build (nanomsg and thrift are installed locally)

--- build/travis/install-nanomsg.sh
+++ build/travis/install-nanomsg.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -e
-wget http://download.nanomsg.org/nanomsg-0.5-beta.tar.gz
-tar -xzvf nanomsg-0.5-beta.tar.gz
-cd nanomsg-0.5-beta
-./configure
-# ./configure --prefix=$HOME/nanomsg
-make && sudo make install
-cd ..
+# check to see if nanomsg folder is empty
+if [ ! -d "$HOME/nanomsg/lib" ]; then
+    wget http://download.nanomsg.org/nanomsg-0.5-beta.tar.gz;
+    tar -xzvf nanomsg-0.5-beta.tar.gz;
+    cd nanomsg-0.5-beta;
+    ./configure --prefix=$HOME/nanomsg;
+    make && make install;
+    cd ..;
+else
+    echo 'Using cached nanomsg directory.'
--- build/travis/install-thrift.sh
+++ build/travis/install-thrift.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -e
-wget http://archive.apache.org/dist/thrift/0.9.2/thrift-0.9.2.tar.gz
-tar -xzvf thrift-0.9.2.tar.gz
-cd thrift-0.9.2
-./configure --with-cpp=yes --with-c_glib=no --with-java=no --with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no
-# ./configure --prefix=$HOME/thrift --with-cpp=yes --with-c_glib=no --with-java=no --with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no
-make && sudo make install
-cd ..
+# check to see if thrift folder is empty
+if [ ! -d "$HOME/thrift/lib" ]; then
+    wget http://archive.apache.org/dist/thrift/0.9.2/thrift-0.9.2.tar.gz;
+    tar -xzvf thrift-0.9.2.tar.gz;
+    cd thrift-0.9.2;
+    ./configure --prefix=$HOME/thrift --with-cpp=yes --with-c_glib=no --with-java=no --with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no;
+    make && make install;
+    cd ..;
+else
+    echo 'Using cached thrift directory.'
