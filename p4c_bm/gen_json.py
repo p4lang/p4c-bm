3541edba3892eb8c7abc8a6098f904fe6498a9a0
Antonin Bas <antonin@barefootnetworks.com>
2016-10-17 12:41:55 -0700

    Add default_action test P4 program (#70)
    Now that support for the default_action attribute has been added to the
    frontend (p4-hlir), I am adding a unit test program and enabling test
    coverage for that part of the code.

--- p4c_bm/gen_json.py
+++ p4c_bm/gen_json.py
@@ -762,7 +762,7 @@ def dump_one_pipeline(json_dict, name, pipe_ptr, hlir):
 
         # temporarily not covered by tests, because not part of P4 spec
         if hasattr(table, "default_action") and\
-           table.default_action is not None:  # pragma: no cover
+           table.default_action is not None:
             LOG_INFO("you are using the default_entry table attribute, "
                      "this is still an experimental feature")
             action, data = table.default_action
